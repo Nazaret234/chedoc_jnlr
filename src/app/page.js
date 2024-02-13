a8639078ae4a673512ca4307c2d7eb6eebe6479c
Nazaret234 <jesusdenazaret05241@gmail.com>
2024-02-12 21:33:28 -0600

    commit footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,16 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import styles from "@/css/page.module.css";
 
-export default function header() {
- return (
-  <body className={styles.body}>
-    <div className={styles.container}>
-      <h1>Demo Streaming</h1>
-      <div>
-        <button className={styles.buttont}>Login</button>
-        <button className={styles.button}>Start you free trial</button>
-      </div>
-    </div>
-    <div className={styles.boxBlack}>
-      <h2>
-        Popular Titles
-      </h2>
-    </div>
-  </body>
+export default function page() {
+  return (
+    <>
+      <body className={styles.body}>
+        <div className={styles.container}>
+          <Header />
+        </div>
+        <Footer />
+      </body>
+    </>
   );
 }
